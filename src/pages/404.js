6de68d137e8adb75f8a884f67cf7add1304f8880
ChuyWNexu <jduarte@nexu.mx>
2021-04-10 22:08:55 -0500

    [add]: 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,51 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql, Link } from 'gatsby'
 import { Layout } from '../components/common'
+import "../styles/notFound.scss"
+const NotFoundComponent = ({ data }) => {
+    console.log(data)
+    return (
+        <Layout>
+            <div className="container">
+                <article className="content" style={{ textAlign: `center` }}>
+                    <h1 className="content-title">Error 404</h1>
+                    <section className="content-body">
+                        No pudimos encontrar lo que buscas. Pero aquí hay unas
+                        cuantas páginas que te pueden interesar
+                        <Link to="/">Noticias</Link>
+                        <Link to="/acercade">¿Quiénes somos?</Link>
+                        <div className="image_container">
+                            <img class="notFound_image" src={data.notFound.childImageSharp.fluid.src} />
+                        </div>
+                    </section>
+                </article>
+            </div>
+        </Layout>
+    )
+}
 
-const NotFoundPage = () => (
-    <Layout>
-        <div className="container">
-            <article className="content" style={{ textAlign: `center` }}>
-                <h1 className="content-title">Error 404</h1>
-                <section className="content-body">
-                    Page not found, <Link to="/">return home</Link> to start over
-                </section>
-            </article>
-        </div>
-    </Layout>
+NotFoundComponent.propTypes = {
+    data: PropTypes.shape({
+        notFound: PropTypes.object.isRequired,
+    }).isRequired,
+}
+
+const NotFoundPage = props => (
+    <StaticQuery
+        query={graphql`
+      query {
+        notFound: file(relativePath: { eq: "errors/Error404.png" }) {
+            childImageSharp{
+                        fluid(maxWidth: 1500){
+                            src
+                        }
+                    }
+                }
+            }
+    `}
+        render={data => <NotFoundComponent data={data} {...props} />}
+    />
 )
 
 export default NotFoundPage
